fcd431fd2a252651b96ff1f57d7d79d0b99d6ba5
Tony Zhang <tzhang995@gmail.com>
2017-07-27 00:20:52 -0400

    Changing to work on snoochat.com

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function(passport) {
   passport.use(new RedditStrategy({
       clientID: "ffmT2Lt4iR6laA",
       clientSecret: "iYZk7wr9uoa6KElYnA6u0PYtE1A",
-      callbackURL: "http://localhost:8080/auth/reddit/callback"
+      callbackURL: "http://snoochat.com/auth/reddit/callback"
     },
     function(accessToken, refreshToken, profile, done) {
       process.nextTick(function() {
